0e3d02d79e4dbcd654d35732c14c17c623978354
drechsv <drechsv@cuni.cz>
2024-11-29 02:43:34 +0100

    Working concept - More styling and fitnfinish ahead: SQLite, encapsulation, etc.

--- Invoices/Invoice.cs
+++ Invoices/Invoice.cs
@@ -14,7 +14,7 @@ public record struct Invoice {
 	
 	public BankInfo BankInfo { get; set; }
 	
-	public OrderInfo OrderInfo { get; set; }
+	public List<OrderInfo> OrdersInfo { get; set; }
 	
 	public List<InvoiceItem> Items { get; set; }
 
@@ -37,6 +37,14 @@ public record struct Invoice {
             BankInfo = new BankInfo {
 	            Name = "Moneta Money Bank",
 	            Account = "000000000000/0600",
+	            QRCode = new QRCode {
+		            Account = "CZ0506000000000269816192+AGBACZPP",
+		            Amount = 147.12m,
+		            Currency = "CZK",
+		            RecipientName = "Vladimír Drechsler",
+		            MessageForRecipient = "Test",
+		            VariableSymbol = 135
+	            }
             },
             VariableSymbol = "0001124",
             CustomerInfo = new PartyInfo {
@@ -70,11 +78,18 @@ public record struct Invoice {
                		Quantity = 12,
                	}
             ],
-            OrderInfo = new OrderInfo {
-               	Number = "0001124",
-               	Date = DateOnly.FromDateTime(DateTime.Now.Subtract(TimeSpan.FromDays(2))),
-               	Delivery = "E-mail",
-            }
+            OrdersInfo = [
+	            new OrderInfo {
+		            Number = "0001124",
+		            Date = DateOnly.FromDateTime(DateTime.Now.Subtract(TimeSpan.FromDays(2))),
+		            Delivery = "E-mail",
+	            },
+	            new OrderInfo {
+		            Number = "0001125",
+		            Date = DateOnly.FromDateTime(DateTime.Now.Subtract(TimeSpan.FromDays(4))),
+		            Delivery = "PPL",
+	            }
+            ]
 		};
 	}
 }
@@ -102,6 +117,8 @@ public record struct Address {
 public record struct BankInfo {
 	public string Name { get; set; }
 	public string Account { get; set; }
+	
+	public QRCode? QRCode { get; set; } 
 }
 
 public record struct OrderInfo {
---
+++ Invoices/QRCode.cs
@@ -0,0 +1,83 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+using Force.Crc32;
+using QRCoder.Core;
+
+namespace PDFMaker.Invoices;
+
+public class QRCode {
+	[MaxLength(46)]
+	public string Account { get; set; } //ACC
+	[MaxLength(93)]
+	public string? AlternativeAccount { get; set; } //ALT-ACC
+	
+	[Column(TypeName = "decimal(10, 2)")]
+	public decimal? Amount { get; set; } //AM
+	[Length(3, 3)]
+	public string? Currency { get; set; } //CC
+	
+	[MaxLength(16)]
+	public int? VariableSymbol { get; set; } //X-VS
+	
+	[MaxLength(35)]
+	public string? RecipientName { get; set; } //RN
+	
+	[MaxLength(60)]
+	public string? MessageForRecipient { get; set; } //MSG
+	
+	private Dictionary<string, string> _data = new();
+	
+	public string GetSPD() {
+		_data = new();
+
+		_data["ACC"] = Account;
+
+		if (AlternativeAccount != null) {
+			_data["ALT-ACC"] = AlternativeAccount;
+		}
+		
+		if (Amount != null) {
+			_data["AM"] = Amount.ToString();
+		}
+		
+		if (Currency != null) {
+			_data["CC"] = Currency;
+		}
+		
+		if (MessageForRecipient != null) {
+			_data["MSG"] = MessageForRecipient;
+		}
+		
+		if (RecipientName != null) {
+			_data["RN"] = RecipientName;
+		}
+		
+		if (VariableSymbol != null) {
+			_data["X-VS"] = VariableSymbol.ToString();
+		}
+
+		var _sortedData = _data
+			.OrderBy(x => x.Key)
+			.ThenBy(x => x.Value);
+
+		string spdData = "";
+		foreach (var (key, value) in _sortedData) {
+			spdData += $"{key}:{value}*";
+		}
+		
+		var _spdBytes = Encoding.UTF8.GetBytes(spdData);
+		string crc = $"CRC32:{Crc32Algorithm.Compute(_spdBytes):X8}";
+		
+		return "SPD*1.0*" + spdData + crc;
+	}
+
+	public string GetQRCode() {
+		using var qrGenerator = new QRCodeGenerator();
+		var qrCodeData = qrGenerator.CreateQrCode(GetSPD(), QRCodeGenerator.ECCLevel.Q);
+		var qrCode = new PngByteQRCode(qrCodeData);
+		var graphic = qrCode.GetGraphic(20);
+		
+		return $"data:image/png;base64,{Convert.ToBase64String(graphic)}";
+	}
+}
