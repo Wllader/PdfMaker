6000f69faf0e171c71c5571a997f7405f41d12e7
wllader <wllader.game@seznam.cz>
2025-07-15 06:13:17 +0200

    Moved Dummy invoice methods to separate file: _InvoiceExtensions.cs

--- Data/DbModel/Invoice.cs
+++ Data/DbModel/Invoice.cs
@@ -36,88 +36,15 @@ public class Invoice : TimeStampedEntity {
 	public List<OrderInfo> OrdersInfo { get; set; } = [];
 	public List<InvoiceItem> Items { get; set; } = [];
 
+	public decimal TotalPrice => Items.Sum(i => i.TotalPrice);
+
 	[NotMapped]	
 	public QrPayment? QrPayment { get; set; }
 	
 	[MaxLength(512)]
 	public string? QrSpr { get; set; }
 	
-	public static Invoice GetTestInvoice() {
-		return new Invoice {
-            Number = "0001124",
-            QrPayment = new QrPayment {
-	            Account = "CZ0506000000000269816192+AGBACZPP",
-	            Amount = 147.12m,
-	            Currency = "CZK",
-	            RecipientName = "Vladimír Drechsler",
-	            MessageForRecipient = "Test",
-	            VariableSymbol = 135
-            },
-            SellerInfo = new PartyInfo {
-               	FirstName = "Aleš",
-                LastName = "Ondráček",
-               	Address = new Address {
-	                Street = "Pražská 92, Ondřejovice",
-	                ZipCode = "331 05",
-	                City = "Ondřejovice",
-	                Country = "Česká republika",
-	                State = "Středočeský kraj",
-                },
-               	VatId = "CZ123456789",
-                Email = "ales.ondracek@ondracek.eu",
-                Phone = "+420 510 045 555"
-            },
-            BankInfo = new BankInfo {
-	            Name = "Moneta Money Bank",
-	            Account = "000000000000/0600",
-            },
-            VariableSymbol = "0001124",
-            CustomerInfo = new PartyInfo {
-               	FirstName = "Jan",
-                LastName = "Novák",
-               	Address = new Address {
-	                Street = "Plzeňská 43/50",
-	                ZipCode = "101 00",
-	                City = "Praha",
-	                Country = "Česká republika",
-                },
-               	VatId = "CZ123456789",
-                Email = "this.is.just@a.test.com",
-                Phone = "+420 456 789 000"
-            },
-            DueDate = DateOnly.FromDateTime(DateTime.Now.AddDays(14)),
-            IssueDate = DateOnly.FromDateTime(DateTime.Now.Subtract(TimeSpan.FromDays(2))),
-            Items = [
-               	new InvoiceItem {
-               		Name = "Faktura",
-               		Description = "Chtěl bych jen vědět, co se stane, když tady bude opravdu, ale vážně opravdu dlouhý popis.",
-               		Unit = "ks",
-               		PricePerUnit = 250,
-               		Quantity = 4,
-               	},
-           
-               	new InvoiceItem {
-               		Name = "PDF",
-               		Description = "Generování PDF",
-               		Unit = "ks",
-               		PricePerUnit = 100,
-               		Quantity = 12,
-               	}
-            ],
-            OrdersInfo = [
-	            new OrderInfo {
-		            Number = "0001124",
-		            Date = DateOnly.FromDateTime(DateTime.Now.Subtract(TimeSpan.FromDays(2))),
-		            Delivery = "E-mail",
-	            },
-	            new OrderInfo {
-		            Number = "0001125",
-		            Date = DateOnly.FromDateTime(DateTime.Now.Subtract(TimeSpan.FromDays(4))),
-		            Delivery = "PPL",
-	            }
-            ],
-		};
-	}
+	
 }
 
 public class InvoiceConfiguration : IEntityTypeConfiguration<Invoice> {
@@ -135,3 +62,4 @@ public class InvoiceConfiguration : IEntityTypeConfiguration<Invoice> {
 			.OnDelete(DeleteBehavior.Restrict);
 	}
 }
+
---
+++ Data/DbModel/InvoiceExtensions.cs
@@ -0,0 +1,87 @@
+namespace Data.DbModel;
+
+public static partial class InvoiceExtensions {
+	public static DateOnly NOW => DateOnly.FromDateTime(DateTime.Now);
+	public static DateOnly DUE(int days) => NOW.AddDays(days);
+	
+	public static Invoice GetTestInvoice() {
+		return new Invoice {
+            Number = "0001124",
+            QrPayment = new QrPayment {
+	            // Account = "CZ0506000000000269816192+AGBACZPP",
+	            // Account = "CZ0506000000000269816192",
+	            Account = "269816192",
+	            BankNumber = "0600",
+	            Amount = 147.12m,
+	            Currency = "CZK",
+	            RecipientName = "Vladimír Drechsler",
+	            MessageForRecipient = "Test",
+	            VariableSymbol = 135
+            },
+            SellerInfo = new PartyInfo {
+               	FirstName = "Aleš",
+                LastName = "Ondráček",
+               	Address = new Address {
+	                Street = "Pražská 92, Ondřejovice",
+	                ZipCode = "331 05",
+	                City = "Ondřejovice",
+	                Country = "Česká republika",
+	                State = "Středočeský kraj",
+                },
+               	VatId = "CZ123456789",
+                Email = "ales.ondracek@ondracek.eu",
+                Phone = "+420 510 045 555"
+            },
+            BankInfo = new BankInfo {
+	            Name = "Moneta Money Bank",
+	            Account = "000000000000",
+	            BankNumber = "1111"
+            },
+            VariableSymbol = "0001124",
+            CustomerInfo = new PartyInfo {
+               	FirstName = "Jan",
+                LastName = "Novák",
+               	Address = new Address {
+	                Street = "Plzeňská 43/50",
+	                ZipCode = "101 00",
+	                City = "Praha",
+	                Country = "Česká republika",
+                },
+               	VatId = "CZ123456789",
+                Email = "this.is.just@a.test.com",
+                Phone = "+420 456 789 000"
+            },
+            DueDate = DateOnly.FromDateTime(DateTime.Now.AddDays(14)),
+            IssueDate = DateOnly.FromDateTime(DateTime.Now.Subtract(TimeSpan.FromDays(2))),
+            Items = [
+               	new InvoiceItem {
+               		Name = "Faktura",
+               		Description = "Chtěl bych jen vědět, co se stane, když tady bude opravdu, ale vážně opravdu dlouhý popis.",
+               		Unit = "ks",
+               		PricePerUnit = 250,
+               		Quantity = 4,
+               	},
+           
+               	new InvoiceItem {
+               		Name = "PDF",
+               		Description = "Generování PDF",
+               		Unit = "ks",
+               		PricePerUnit = 100,
+               		Quantity = 12,
+               	}
+            ],
+            OrdersInfo = [
+	            new OrderInfo {
+		            Number = "0001124",
+		            Date = DateOnly.FromDateTime(DateTime.Now.Subtract(TimeSpan.FromDays(2))),
+		            Delivery = "E-mail",
+	            },
+	            new OrderInfo {
+		            Number = "0001125",
+		            Date = DateOnly.FromDateTime(DateTime.Now.Subtract(TimeSpan.FromDays(4))),
+		            Delivery = "PPL",
+	            }
+            ],
+		};
+	}
+}
